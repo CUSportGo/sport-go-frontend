a00879df842f2532b9bcd3ae248f695c45275260
KnotPP <88419066+KnotPP@users.noreply.github.com>
2023-09-13 20:55:35 +0700

    init v2
    -change npm to yarn
    -add antd
    -create page and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import LoginPage from './pages/LoginPage/LoginPage';
+import RegisterPage from './pages/RegisterPage/RegisterPage';
 
 function App() {
   return (
     <div>
-      app
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
     </div>
   );
 }
