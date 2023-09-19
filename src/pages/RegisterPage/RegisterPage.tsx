2bc62b4c076b75c7086f09f06716578798fe0093
KnotPP <88419066+KnotPP@users.noreply.github.com>
2023-09-19 21:36:48 +0700

    form add all field and css

--- src/pages/RegisterPage/RegisterPage.tsx
+++ src/pages/RegisterPage/RegisterPage.tsx
@@ -1,45 +1,230 @@
 import { useState } from "react";
 import "./RegisterPage.css";
 import { Button, Form, Input, message } from "antd";
+import Upload, {
+  RcFile,
+  UploadChangeParam,
+  UploadFile,
+  UploadProps,
+} from "antd/es/upload";
+import { LoadingOutlined, PlusOutlined } from "@ant-design/icons";
+import { NavLink } from "react-router-dom";
+
+const getBase64 = (img: RcFile, callback: (url: string) => void) => {
+  const reader = new FileReader();
+  reader.addEventListener("load", () => callback(reader.result as string));
+  reader.readAsDataURL(img);
+};
+
+const beforeUpload = (file: RcFile) => {
+  const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
+  if (!isJpgOrPng) {
+    message.error("You can only upload JPG/PNG file!");
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2;
+  if (!isLt2M) {
+    message.error("Image must smaller than 2MB!");
+  }
+  return isJpgOrPng && isLt2M;
+};
 
 const RegisterPage = () => {
+  const [loading, setLoading] = useState(false);
+  const [imageUrl, setImageUrl] = useState<string>();
+
+  const handleChange: UploadProps["onChange"] = (
+    info: UploadChangeParam<UploadFile>
+  ) => {
+    console.log(info.file);
+    
+    if (info.file.status === "uploading") {
+      setLoading(true);
+      return;
+    }
+    if (info.file.status === "done") {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj as RcFile, (url) => {
+        setLoading(false);
+        setImageUrl(url);
+      });
+    }
+  };
+
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div style={{ marginTop: 8 }}>Upload</div>
+    </div>
+  );
+
+  const validatePhoneFormat = (
+    _: any,
+    value: string,
+    callback: (message?: string) => void
+  ) => {
+    const phoneRegex = /^\d{3}-\d{3}-\d{4}$/; // Regular expression for xxx-xxx-xxxx format
+
+    if (!phoneRegex.test(value) && value !== "" && value !== undefined) {
+      callback("Phone number should be in the format xxx-xxx-xxxx");
+    } else {
+      callback(); // Validation passed
+    }
+  };
+
   return (
     <div className="registerPage-mainPage">
+      <div className="LoginPage-logoName">
+        SPORT <span className="go-text"> GO</span>
+      </div>
       <div className="form-container">
         <Form
           style={{
             width: 250,
             margin: "auto",
-            // display: "flex",
-            // flexDirection: "column",
-            // alignItems: "center",
+            fontFamily: "Poppins",
           }}
           layout="vertical"
         >
           <div className="register-n-image">
-            <h1 className="register-label">Register</h1>
+            <p className="register-label">Register</p>
+            <div className="upload-image">
+              <Upload
+                name="avatar"
+                listType="picture-circle"
+                className="avatar-uploader"
+                showUploadList={false}
+                action="https://run.mocky.io/v3/435e224c-44fb-4773-9faf-380c5e6a2188"
+                beforeUpload={beforeUpload}
+                onChange={handleChange}
+              >
+                {imageUrl ? (
+                  <img
+                    src={imageUrl}
+                    alt="avatar"
+                    style={{
+                      width: "100%",
+                      height: "100%",
+                      borderRadius: "50%",
+                      objectFit: "cover",
+                    }}
+                  />
+                ) : (
+                  uploadButton
+                )}
+              </Upload>
+            </div>
           </div>
-          <Form.Item label="Firstname" name="firstname">
+
+          <Form.Item
+            label="Firstname"
+            name="firstname"
+            style={{ marginBottom: "8px" }}
+            rules={[
+              {
+                required: true,
+                message: "Please input your Firstname",
+              },
+            ]}
+          >
             <Input placeholder="Firstname" />
           </Form.Item>
-          <Form.Item label="Lastname" name="lastname">
+          <Form.Item
+            label="Lastname"
+            name="lastname"
+            style={{ marginBottom: "8px" }}
+            rules={[
+              {
+                required: true,
+                message: "Please input your Lastname",
+              },
+            ]}
+          >
             <Input placeholder="Lastname" />
           </Form.Item>
-          <Form.Item label="Email" name="email">
+          <Form.Item
+            label="Email"
+            name="email"
+            style={{ marginBottom: "8px" }}
+            rules={[
+              {
+                type: "email",
+                message: "The input is not valid E-mail!",
+              },
+              {
+                required: true,
+                message: "Please input your E-mail",
+              },
+            ]}
+          >
             <Input placeholder="Email" />
           </Form.Item>
-          <Form.Item label="Phone number" name="Phonenumber">
+          <Form.Item
+            label="Phone number"
+            name="Phonenumber"
+            style={{ marginBottom: "8px" }}
+            rules={[
+              {
+                required: true,
+                message: "Please input your Phone number",
+              },
+              {
+                validator: validatePhoneFormat,
+              },
+            ]}
+          >
             <Input placeholder="Phone number" />
           </Form.Item>
-          <Form.Item label="Password" name="password">
+          <Form.Item
+            label="Password"
+            name="password"
+            style={{ marginBottom: "8px" }}
+            rules={[
+              {
+                required: true,
+                message: "Please input your Password",
+              },
+            ]}
+          >
             <Input.Password placeholder="Password" />
           </Form.Item>
-          <Form.Item label="Confirm password" name="confirmpassword">
+          <Form.Item
+            label="Confirm password"
+            name="confirmpassword"
+            style={{ marginBottom: "28px" }}
+            rules={[
+              {
+                required: true,
+                message: "Please confirm your password",
+              },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue("password") === value) {
+                    return Promise.resolve();
+                  }
+                  return Promise.reject(
+                    new Error("The password that you entered do not match")
+                  );
+                },
+              }),
+            ]}
+          >
             <Input.Password placeholder="Confirm password" />
           </Form.Item>
-          <Form.Item>
-            <Button>Register</Button>
+          <Form.Item
+            name="registerbutton"
+            className="register-item-button"
+            style={{ marginBottom: "11px" }}
+          >
+            <Button className="register-button" htmlType="submit">
+              Register
+            </Button>
           </Form.Item>
+          <p className="RegisterPage-logintext">
+            Already have an account?{" "}
+            <NavLink to="/login" className="login-text">
+              login
+            </NavLink>
+          </p>
         </Form>
       </div>
     </div>
