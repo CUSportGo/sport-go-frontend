c4694d482b53f696d31ae95e29eac408739d7b84
pawarisble <blebikebe@gmail.com>
2023-10-02 21:16:20 +0700

    add forgot password page

---
+++ src/pages/ForgotPasswordPage/ForgotPasswordPage.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import "./ForgotPasswordPage.css";
+import { MdArrowBackIosNew } from "react-icons/md";
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Buttons/Button";
+
+const ForgotPasswordPage = () => {
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const handleBackButton = () => {
+    navigate("/login");
+  };
+
+  const handleSendEmail = () => {
+    alert("Email was sent!");
+  };
+
+  return (
+    <div className="ForgotPassword-mainPage">
+      <div className="ForgotPassword-container">
+        <div className="ForgotPassword-backButton" onClick={handleBackButton}>
+          <MdArrowBackIosNew style={{ color: "white" }} />
+        </div>
+
+        <form className="ForgotPassword-box">
+          <div className="ForgotPassword-text">Forgot Password</div>
+          <div className="ForgotPassword-emailText"> Email</div>
+
+          <input
+            className="ForgotPassword-emailForm"
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <div className="ForgotPassword-Button">
+            <Button
+              message={"Send Email"}
+              onClick={handleSendEmail}
+              bheight="40px"
+              bwidth="200px"
+              fontSize="16px"
+              borderRadius="5px"
+              bgColor="#5729CE"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ForgotPasswordPage;
