436499470d65c75d666eeb4dfb0318fc9427c0aa
KnotPP <88419066+KnotPP@users.noreply.github.com>
2023-10-31 22:50:41 +0700

    init page

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import ResetPasswordPage from "./pages/ResetPasswordPage/ResetPasswordPage";
 import "./App.css";
 import ResetPasswordSuccessfully from "./pages/ResetPasswordPage/ResetPasswordSuccessfully";
 import ForgotPasswordPage from "./pages/ForgotPasswordPage/ForgotPasswordPage";
+import CreateSportAreaPage from "./pages/CreateSportAreaPage/CreateSportAreaPage";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
         <Route path="/reset-password/:token" element={<ResetPasswordPage />} />
         <Route path="/resetsuc" element={<ResetPasswordSuccessfully />} />
+        <Route path="/create-sportarea" element={<CreateSportAreaPage/>} />
       </Routes>
     </div>
   );
---
+++ src/pages/CreateSportAreaPage/CreateSportAreaPage.tsx
@@ -0,0 +1,11 @@
+import "./CreateSportAreaPage.css";
+
+const CreateSportAreaPage = () => {
+    return (
+        <div className="create-sportarea-container">
+            CreateSportAreaForm
+        </div>
+    );
+};
+
+export default CreateSportAreaPage;
